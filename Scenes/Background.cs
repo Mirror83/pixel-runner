94841a9f4b9629db5e3ecf88b179faf953f1b076
Mirror83 <glenochieng045@gmail.com>
2023-12-27 10:03:51 +0300

    Renamed _backgroundVelocity to _scrollVelocity

--- Scenes/Background.cs
+++ Scenes/Background.cs
@@ -1,11 +1,10 @@
 using Godot;
-using System;
 
 namespace PixelRunner.Scenes;
 
 public partial class Background : Control
 {
-	private Vector2 _backgroundVelocity = new(-80f, 0);
+	private Vector2 _scrollVelocity = new(-80f, 0);
 
 	private Vector2 _screenSize;
 	// Called when the node enters the scene tree for the first time.
@@ -17,7 +16,7 @@ public partial class Background : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Position += _backgroundVelocity * (float)delta;
+		Position += _scrollVelocity * (float)delta;
 		if (Position.X >= -_screenSize.X) return;
 		Position = new Vector2(0, Position.Y);
 	}
