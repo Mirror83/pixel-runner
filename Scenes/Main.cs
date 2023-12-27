5687799b52314a43fbdf0347eff4d44ee691d256
Mirror83 <glenochieng045@gmail.com>
2023-12-27 11:11:23 +0300

    Added ability to spawn flies

--- Scenes/Main.cs
+++ Scenes/Main.cs
@@ -9,6 +9,9 @@ public partial class Main : Node
 
     private int _score;
 
+    private const int FlyThresholdScore = 10;
+    // private const int ScoreMultiplier = 100;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -22,65 +25,70 @@ public partial class Main : Node
 
     private void OnEnemySpawnTimerTimeout()
     {
-        var snail = SnailScene.Instantiate<Snail>();
+        var isFly = false;
+        if (_score >= FlyThresholdScore)
+            isFly = GD.Randi() % 2 == 0;
 
-        var spawnPosition = GetNode<Marker2D>("SnailSpawnPosition").Position;
-        snail.Position = spawnPosition;
+        var enemy = isFly ? FlyScene.Instantiate<Fly>() : SnailScene.Instantiate<Snail>();
+
+        var spawnPosition = isFly
+            ? GetNode<Marker2D>("FlySpawnPosition").Position
+            : GetNode<Marker2D>("SnailSpawnPosition").Position;
+        enemy.Position = spawnPosition;
 
         // The x component is negative as the snail is supposed to move to the left side of
         // the screen. The y component is zero as it should move along the ground, which is not sloped.
         var velocity = new Vector2(-300, 0);
-        snail.LinearVelocity = velocity;
+        enemy.LinearVelocity = velocity;
 
-        AddChild(snail);
+        AddChild(enemy);
     }
 
     private void OnNewGame()
     {
         var player = GetNode<Player>("Player");
-        
+
         // Enable player physics
         player.ProcessMode = ProcessModeEnum.Inherit;
-        
+
         if (!player.Visible) player.Show();
-        
+
         // (Re)start enemy spawn timer
         GetNode<Timer>("EnemySpawnTimer").Start();
-       
+
         // Enable physics for the snails
-        GetTree().SetGroup("snails", Node.PropertyName.ProcessMode, (int)ProcessModeEnum.Inherit);
-        
+        GetTree().SetGroup("enemies", Node.PropertyName.ProcessMode, (int)ProcessModeEnum.Inherit);
+
         // Enable ground and background scrolling
         GetNode<GroundTexture>("Ground/GroundTexture").SetProcess(true);
         GetNode<Background>("Background").SetProcess(false);
 
-        
-        GetNode<Timer>("ScoreTimer").Stop();
 
-        
+        GetNode<Timer>("ScoreTimer").Stop();
     }
+
     private async void OnPlayerHit()
     {
         GD.Print("Game over");
-        
+
         var enemySpawnTimer = GetNode<Timer>("EnemySpawnTimer");
         enemySpawnTimer.Stop();
-        
+
         GetNode<GroundTexture>("Ground/GroundTexture").SetProcess(false);
-        
+
         GetNode<Background>("Background").SetProcess(false);
-        
+
         GetNode<Timer>("ScoreTimer").Stop();
-        
-        GetTree().SetGroup("snails", Node.PropertyName.ProcessMode, (int)ProcessModeEnum.Disabled);
+
+        GetTree().SetGroup("enemies", Node.PropertyName.ProcessMode, (int)ProcessModeEnum.Disabled);
 
         var player = GetNode<Player>("Player");
         player.ProcessMode = ProcessModeEnum.Disabled;
-        
+
         await ToSignal(GetTree().CreateTimer(2.0f), SceneTreeTimer.SignalName.Timeout);
 
-        GetTree().CallGroup("snails", Node.MethodName.QueueFree);
-        
+        GetTree().CallGroup("enemies", Node.MethodName.QueueFree);
+
         player.Hide();
     }
 
